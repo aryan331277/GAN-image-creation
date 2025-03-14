c6aa3a347e1eecb079e3cd298c7f4e1ac59c519f
aryan331277 <bt24csd009@iiitn.ac.in>
2025-03-14 23:31:36 +0530

    Update running_gan.py

--- running_gan.py
+++ running_gan.py
@@ -1,6 +1,12 @@
 dcgan=DCGAN(generator=generator,discriminator=discriminator,latent_dim=LATENT_DIM)
-D_LR=0.0001
-G_LR=0.0003
-dcgan.compile(g_optimizer=Adam(learning_rate=G_LR,beta_1=0.5),d_optimizer=Adam(learning_rate=D_LR,beta_1=0.5),loss_fn=BinaryCrossentropy())
-N_EPOCHS=50
-dcgan.fit(train_images,epochs=N_EPOCHS,callbacks=[DCGANMonitor()])
+D_LR = 2e-4  
+G_LR = 1e-4  
+dcgan.compile(
+    g_optimizer=Adam(learning_rate=G_LR, beta_1=0.5), 
+    d_optimizer=Adam(learning_rate=D_LR, beta_1=0.5), 
+    loss_fn=BinaryCrossentropy(from_logits=True)
+)
+
+# Train with Monitoring
+N_EPOCHS = 200
+dcgan.fit(train_images, epochs=N_EPOCHS, callbacks=[DCGANMonitor()])
